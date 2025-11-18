1bf2476767485a9f82eed26bc27165dbc061e4af
root <root@DESKTOP-PLCA8BP.localdomain>
2025-11-18 17:45:58 +0100

    fix la boucle dimport

--- part3/app/models/user.py
+++ part3/app/models/user.py
@@ -1,7 +1,8 @@
-from app import bcrypt, db
+from app import db
 from .basemodel import BaseModel
 import re
 from sqlalchemy.orm import validates
+
 
 class User(BaseModel):
     __tablename__ = 'users'
@@ -18,7 +19,8 @@ class User(BaseModel):
         if not isinstance(value, str):
             raise TypeError("First name must be a string")
         if len(value) > 50:
-            raise ValueError("First name must be less than or equal to 50 characters")
+            raise ValueError(
+                "First name must be less than or equal to 50 characters")
         return value
 
     @validates('password')
@@ -28,7 +30,7 @@ class User(BaseModel):
         if len(value) < 8:
             raise ValueError("Password must be at least 8 characters long")
         return self.hash_password(value)
-    
+
     @validates('email')
     def validate_email(self, key, value):
         if not isinstance(value, str):
@@ -50,9 +52,11 @@ class User(BaseModel):
 
     def hash_password(self, password):
         """Hashes the password before storing it."""
+        from app import bcrypt
         return bcrypt.generate_password_hash(password).decode('utf-8')
-    
+
     def verify_password(self, password):
+        from app import bcrypt
         return bcrypt.check_password_hash(self.password, password)
 
     def add_place(self, place):
